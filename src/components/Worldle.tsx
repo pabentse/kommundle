9c4c224532c945fd9c096048422de8443f608255
oyvindbso <oyvindbso@users.noreply.github.com>
2023-03-19 21:25:35 +0100

    fjernet litt oversettelse og la til kommundle

--- src/components/Worldle.tsx
+++ src/components/Worldle.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Worldle() {
   return (
     <span className="font-bold">
-      WOR<span className="text-green-600">L</span>DLE
+      KOMMUND<span className="text-green-600">L</span>E
     </span>
   );
 }
