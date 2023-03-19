074509ef828af14896b8d842daf279e6fefb0b0b
Øyvind Bugge Solheim <oyvinbso@uio.no>
2023-03-19 21:49:27 +0100

    argh en til parentes

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -65,7 +65,7 @@ export function Share({
       }}
     >
       <button className="border-2 px-4 uppercase bg-green-600 hover:bg-green-500 active:bg-green-700 text-white w-full">
-        {"Del")}
+        {"Del"}
       </button>
     </CopyToClipboard>
   );
