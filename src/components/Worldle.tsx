ffdb90be59e3c563273b642cda79f98a65f554b0
pabentse <pabentse@ifi.uio.no>
2023-06-13 20:32:46 +0200

    Updates

--- src/components/Worldle.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Worldle() {
-  return (
-    <span className="font-bold">
-      KOMMUND<span className="text-green-600">L</span>E
-    </span>
-  );
-}
